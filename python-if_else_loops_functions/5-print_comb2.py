f07ae143b5c1d3f845a4dc1460a453b4cf22249e
LachyBM <13072@holbertonstudents.com>
2026-05-05 16:01:44 +0000

    changed if statement and using .format

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for n in range (0,100):
-    if n != 99:
-        print(f"{n:02}, ", end ="")
-    else: 
-        print(f"{n:02}")   
+    if n < 10:
+        print("0{}, ".format(n), end ="")
+    elif n < 99: 
+        print("{}, ".format(n), end = "")
+    else:
+        print("{}".format(n))
