bff344026fea2f2c40621ff2e2b6f757d6410395
LachyBM <13072@holbertonstudents.com>
2026-05-05 16:02:47 +0000

    fixed to many print functions

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for n in range (0,100):
-    if n < 10:
-        print("0{}, ".format(n), end ="")
-    elif n < 99: 
-        print("{}, ".format(n), end = "")
+    if n < 99: 
+        print("{:02}, ".format(n), end = "")
     else:
         print("{}".format(n))
