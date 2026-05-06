bb2b1dd3c2e7277476220bbefc9cd0cb6227dacd
LachyBM <13072@holbertonstudents.com>
2026-05-06 06:42:43 +0000

    fixed spacing

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,8 +6,7 @@ count = len(argv)
 
 if __name__ == "__main__":
     final = 0
-    for n in range (1, count):
+    for n in range(1, count):
         final += int(argv[n])
 
     print("{}".format(final))
-
