e4edb1059a4271685936fae4d8a072750a83c506
LachyBM <13072@holbertonstudents.com>
2026-05-06 06:34:17 +0000

    fixed spacing and 0 arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,8 +5,10 @@ count = len(argv)
 if __name__ == "__main__":
     if count == 2 :
         print("1 argument:")
+    elif count == 1:
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(count-1))
+        print("{} arguments:".format(count - 1))
 
     for n in range(1, count):
-        print("{}: {}".format(n,argv[n]))
+        print("{}: {}".format(n, argv[n]))
