c8fcca2710204ce7a4e5badfdddede34b00713d1
LachyBM <13072@holbertonstudents.com>
2026-05-09 15:17:55 +0000

    fixed whitespacing

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for n in range(0, 9):
     for x in range(n + 1, 10):
         if (n, x) == (8, 9):
-            print("{0}{1}".format(n,x))
+            print("{0}{1}".format(n, x))
         else:
             print("{0}{1}".format(n, x), end=", ")
