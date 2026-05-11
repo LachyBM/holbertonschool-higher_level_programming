ee3058cb72c143ca1f3958555493d9d8e46ffb20
LachyBM <13072@holbertonstudents.com>
2026-05-11 06:17:02 +0000

    made return my_list instead of none

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,9 +2,9 @@
 
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return None
-    if idx > len(my_list):
-        return None
+        return my_list
+    if idx >= len(my_list):
+        return my_list 
     else:
        my_list[idx] = element
        return my_list
