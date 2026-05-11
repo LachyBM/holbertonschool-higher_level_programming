9e841544b374b7f7a36e3b64db06d8bd3c4ce226
LachyBM <13072@holbertonstudents.com>
2026-05-11 06:13:22 +0000

    fix? to len

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return None
-    if idx >= len(my_list):
+    if idx > len(my_list):
         return None
     else:
        my_list[idx] = element
