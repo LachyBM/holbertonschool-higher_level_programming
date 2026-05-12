d8d2ecdd234fd5b96c2c74fc837f442daa54f146
LachyBM <13072@holbertonstudents.com>
2026-05-12 03:59:41 +0000

    fixed negative number largest

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    largest = 0 
     if not my_list:
         return None
     for n in range(0,len(my_list)):
-        if my_list[n] > int(largest):
+        largest = my_list[0]
+        if my_list[n] > largest:
             largest = my_list[n]
     return largest
 
