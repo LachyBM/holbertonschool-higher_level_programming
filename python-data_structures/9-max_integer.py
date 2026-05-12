9f8b3ca828d268a332b3c3e7623b38e43d91308b
LachyBM <13072@holbertonstudents.com>
2026-05-12 03:56:27 +0000

    fixed null value

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    largest = 0
+    largest = 0 
+    if not my_list:
+        return None
     for n in range(0,len(my_list)):
-        if my_list[n] > largest:
+        if my_list[n] > int(largest):
             largest = my_list[n]
     return largest
 
