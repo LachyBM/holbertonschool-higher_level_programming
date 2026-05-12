2a00c330ae4c16989b55c9fb655a1ddcf9dcd607
LachyBM <13072@holbertonstudents.com>
2026-05-12 05:39:00 +0000

    indented to much

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -9,5 +9,5 @@ def square_matrix_simple(matrix=[]):
                 new_row.append(None)
             else:
                 new_row.append(n * n)
-                new_matrix.append(new_row)
+        new_matrix.append(new_row)
     return new_matrix
