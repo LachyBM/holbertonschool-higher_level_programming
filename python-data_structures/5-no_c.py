eb650382131a8da98c0b4f20de543587d3a13e50
LachyBM <13072@holbertonstudents.com>
2026-05-12 00:27:45 +0000

    task 5 added

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    result = ""
+    for char in my_string:
+        if char == 'c' or char == 'C':
+            result += ""
+        else:
+            result += char
+    return result 
+            
