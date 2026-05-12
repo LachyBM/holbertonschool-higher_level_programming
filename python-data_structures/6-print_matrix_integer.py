7f65602ef68128efe6c7becf78c14ba51a401535
LachyBM <13072@holbertonstudents.com>
2026-05-12 02:31:33 +0000

    test

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            if element % int(len(row)) == 0:
-                print("{:d}".format(element),end="\n")
+            if element % len(row) == 0:
+                print("{:d}".format(element),end="")
             else:
                 print("{:d}".format(element),end=" ")
+        print()
