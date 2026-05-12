a6a4c2372802e441bea0a6a35040f3db3ad66f8f
LachyBM <13072@holbertonstudents.com>
2026-05-12 04:02:12 +0000

    fixed white spacing

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,8 +4,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     largest = my_list[0]
-    for n in range(0,len(my_list)):
+    for n in range(0, len(my_list)):
         if my_list[n] > largest:
             largest = my_list[n]
-    return largest
-
+            return largest
