34988199bbbe828fd5ae54d2aa7c8103f3daaf4c
LachyBM <13072@holbertonstudents.com>
2026-05-12 04:01:32 +0000

    moved largest intialize

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
+    largest = my_list[0]
     for n in range(0,len(my_list)):
-        largest = my_list[0]
         if my_list[n] > largest:
             largest = my_list[n]
     return largest
