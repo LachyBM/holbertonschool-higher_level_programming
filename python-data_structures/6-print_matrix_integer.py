2b6bb9fa1948122a1c6be5bbc0766cd0c2b20d40
LachyBM <13072@holbertonstudents.com>
2026-05-12 01:21:50 +0000

    fixed spacing at end

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            if element >= (len(row) % element==0):
-                print("{:d}".format(element),end=" ")
+            if element % int(len(row)) == 0:
+                print("{:d}".format(element),end="\n")
             else:
-                print("{:d}".format(element),end="")
-        print()
+                print("{:d}".format(element),end=" ")
