1605e23088f939af15b80eda5c1678d5a6e8e983
LachyBM <13072@holbertonstudents.com>
2026-05-12 03:36:07 +0000

    fixed the printing of none

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
     first = ""
     for char in sentence:
         if char in range(0) == "":
-            first = None
+            first = "None"
         else:
             first = sentence[0]
     return len(sentence), first
