7c6525f2c840e9de37815b3c6c296310d871d3f4
LachyBM <13072@holbertonstudents.com>
2026-05-12 05:14:55 +0000

    checking if none is the problem

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,9 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new_row = []
         for n in row:
-            new_row.append(n * n) 
+            if n == None:
+                new_row.append(None)
+            else:
+                new_row.append(n * n) 
         new_matrix.append(new_row) 
     return new_matrix
