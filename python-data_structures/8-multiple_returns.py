d2ae951db8cf895ec4d9a9ed3a7725ad67269e57
LachyBM <13072@holbertonstudents.com>
2026-05-12 03:38:07 +0000

    changed and got rid of range, and made 2 return cases

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    first = ""
-    for char in sentence:
-        if char in range(0) == "":
-            first = "None"
-        else:
-            first = sentence[0]
-    return len(sentence), first
+    if len(sentence) == 0:
+        return 0, None
+    else:
+        return len(sentence), first
