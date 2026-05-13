bf59d18631bfa22be189aa59e28aa8a224d6b327
LachyBM <13072@holbertonstudents.com>
2026-05-13 05:01:30 +0000

    fixed expect

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for n in range(0, x):
             print("{}".format(my_list[n]), end="")
-    except:
+    except Exception:
         if x > n:
             print()
             return n
