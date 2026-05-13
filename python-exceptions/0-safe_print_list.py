dec4cda537eded4329cb90c7714f78b93d034322
LachyBM <13072@holbertonstudents.com>
2026-05-13 04:42:08 +0000

    fixed whtie spacing?

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for n in range(0,x):
-            print("{}".format(my_list[n]),end="")
+            print("{}".format(my_list[n]), end="")
     except:
         if x > n:
             print()
