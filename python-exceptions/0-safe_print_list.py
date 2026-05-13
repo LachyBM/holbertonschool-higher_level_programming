003e95351a8ef4ccec660b6f83984cd0431921b2
LachyBM <13072@holbertonstudents.com>
2026-05-13 04:46:40 +0000

    white spaces???????

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        for n in range(0,x):
+        for n in range(0, x):
             print("{}".format(my_list[n]), end="")
     except:
         if x > n:
