a5044f1dd188b68625b214fccd8e1b0b54ec532e
LachyBM <13072@holbertonstudents.com>
2026-05-13 04:41:06 +0000

    task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for n in range(0,x):
+            print("{}".format(my_list[n]),end="")
+    except:
+        if x > n:
+            print()
+            return n
+    print()
+    return x
