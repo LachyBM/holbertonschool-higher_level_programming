7818a58109766f95710fef2e694481b1bf121b5b
LachyBM <13072@holbertonstudents.com>
2026-05-13 13:37:37 +0000

    check if not a string and value none

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+
     roman = {"M": 1000,
              "D": 500,
              "C": 100,
