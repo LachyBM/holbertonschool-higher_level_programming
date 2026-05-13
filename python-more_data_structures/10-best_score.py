792b2f23bd351a8cc7bb0be07d75279becaf6ad4
LachyBM <13072@holbertonstudents.com>
2026-05-13 01:30:59 +0000

    fixed white spacing

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if bool(a_dictionary) == False:
+    if bool(a_dictionary) is False:
         return None
     result = max(a_dictionary, key=a_dictionary.get)
     return result
-        
