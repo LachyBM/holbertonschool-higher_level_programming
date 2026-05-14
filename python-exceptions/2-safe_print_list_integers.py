ab29b641d647d5d40bc350cfc3e836ef60175ee8
LachyBM <13072@holbertonstudents.com>
2026-05-14 01:36:19 +0000

    2 finally done, using loop and except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        for n in range(0, x):
-            if int(my_list[n]):
-                print("{:d}".format(my_list[n]), end="")
-            else:
-                print("LOOP")
-    except Exception:
-        if x > n:
-            print()
-            return n
+    count = 0
+    for n in range(0,x):
+        try:
+            print("{:d}".format(my_list[n]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            count -= 0
     print()
-    return x
+    return count
