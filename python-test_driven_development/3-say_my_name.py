cc34acdb42f5a9a9a5e48cfe40167b44f56c4b80
LachyBM <13072@holbertonstudents.com>
2026-05-15 04:11:34 +0000

    task 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module for printing the
+first and last name of a person
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    prints the inputed first and
+    last name making sure
+    both are a string
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name myst be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
