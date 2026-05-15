eb8a913c8e753a43133ed95b50a9ebe099f62c5d
LachyBM <13072@holbertonstudents.com>
2026-05-15 03:45:54 +0000

    fixed div 0

--- python-test_driven_development/2-main.py
+++ python-test_driven_development/2-main.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-    [1, 2, 3],
-    [2, 3, 5]
-]
-print(matrix_divided(matrix, 2))
-print(matrix)
+matrix_divided([[1, 14, 3], [1, 7, 1]], 0)
