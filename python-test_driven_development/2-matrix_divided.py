5b8871a7a4425c2950be05ee88dd1f7728d63046
LachyBM <13072@holbertonstudents.com>
2026-05-15 03:33:24 +0000

    fixed psycode

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,15 +8,17 @@ def matrix_divided(matrix, div):
     """
     divide all numbers in the matrix
     by the number entered
+    em =  error message as without
+    it breaks pycodestyle
     """
 
-
+    em = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(em)
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(em)
     if not all(isinstance(n, (int, float)) for row in matrix for n in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(em)
     if len(set(len(row) for row in matrix)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
