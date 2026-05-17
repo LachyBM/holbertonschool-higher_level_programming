1bf263bc72babf59ed06ff1050bd0e329d8ba48e
LachyBM <13072@holbertonstudents.com>
2026-05-17 00:59:41 +0000

    fixed none

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -25,7 +25,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3, -4]), 3)
 
     def test_empty(self):
-        self.assertEqual(max_integer([]))
+        self.assertIsNone(max_integer([]))
 
 if __name__ == "__main__":
     unittest.main()
