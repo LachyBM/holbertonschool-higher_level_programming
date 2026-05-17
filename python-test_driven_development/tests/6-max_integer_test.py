8ebae3ff10a6f832a8d22f1165dd4973e112c268
LachyBM <13072@holbertonstudents.com>
2026-05-17 00:51:19 +0000

    added test cases

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_standard(self):
+        self.assertEqual(max_integer([1, 2, 10, 3]), 10)
+
+    def test_order(self):
+        self-assertEqual(max_integer([1, 2, 3, 4)], 4)
+
+    def test_single(self):
+        self-assertEqual(max_integer([1], 1))
+
+    def test_negatives(self):
+        self_assertEqual(max_integer([-1, -4, -10, -2], -10))
+
+    def test_one_negative(self):
+        self_assertEqual(max_integer([1, 2, 3, -4], 3))
+
+if __name__ == "__main__":
+    unittest.main()
