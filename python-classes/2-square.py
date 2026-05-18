41f9875deb2dd439b2518125d17bc58c79f81f5d
LachyBM <13072@holbertonstudents.com>
2026-05-18 00:22:22 +0000

    raise error if not values

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module for a square
+"""
+
+
+class Square:
+    """
+    empty sqaure
+    """
+    def __init__(self, size = 0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = size
