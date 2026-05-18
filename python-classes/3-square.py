58e87f333d9784c9c1bc750b7283bafa25b09f45
LachyBM <13072@holbertonstudents.com>
2026-05-18 00:36:37 +0000

    value of area

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+module for a square
+"""
+
+
+class Square:
+    """
+    empty sqaure
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        side = self.__size
+        return (side*side)
