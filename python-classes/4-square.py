7b59a3b5ee1fcfdd7c969c6b067701d4963a1bb6
LachyBM <13072@holbertonstudents.com>
2026-05-18 00:55:51 +0000

    4 square

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+module for a square
+"""
+
+
+class Square:
+    """
+    empty sqaure
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        side = self.__size
+        return (side*side)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
