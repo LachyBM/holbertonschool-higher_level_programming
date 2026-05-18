036fee821c85ca06daefce8db071671316fb3f70
LachyBM <13072@holbertonstudents.com>
2026-05-18 01:11:01 +0000

    works but idk if wanted way

---
+++ python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+module for a square
+"""
+
+
+class Square:
+    """
+    empty sqaure
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        side = self.__size
+        return (side*side)
+
+    def my_print(self):
+        size = self.__size
+
+        if size == 0:
+            print()
+
+        for n in range(size):
+            for n in range(size):
+                print("#", end="")
+            print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
