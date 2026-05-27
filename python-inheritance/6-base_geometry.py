5f279d348215530956ea46d384218dbd7b0a108c
LachyBM <13072@holbertonstudents.com>
2026-05-27 16:03:42 +0000

    added area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module for geometry
+"""
+
+
+class BaseGeometry():
+    """
+    area of a geo
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
