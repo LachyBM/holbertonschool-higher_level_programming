79ca4c64acf08241a23695f58d5c10a55f40af00
LachyBM <13072@holbertonstudents.com>
2026-05-27 15:39:25 +0000

    fixed commenting/the 3 in python3

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""
+Module for printing sorted list
+"""
+
 
 class MyList(list):
+    """
+    get list of numbers (int)
+    and print them sorted
+    """
+
     def print_sorted(self):
         print(sorted(self))
