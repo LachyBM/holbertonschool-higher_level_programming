3ee5ac85e3582fb9c8d95cf3d0fe8cddeac05547
LachyBM <13072@holbertonstudents.com>
2026-05-27 15:53:03 +0000

    quick guesstimate of what the answer would be

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module to check if same class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    return true is kind of same class
+    else false
+    """
+
+    return isinstance(obj, a_class)
