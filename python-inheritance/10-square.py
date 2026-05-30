8d3bd95511f7417d4e9ad88b8ef50357b1dc7b30
LachyBM <13072@holbertonstudents.com>
2026-05-30 03:20:43 +0000

    used actual method

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -3,20 +3,14 @@
 module for geometry
 of a recet
 """
-BaseGeometry = __import__("9-rectangle").BaseGeometry
+Rectangle = __import__("9-rectangle").Rectangle
 
 
-class Square(BaseGeometry):
+class Square(Rectangle):
     """
     rectangle geo class
     """
 
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__size = size
-
-    def area(self):
-        return self.__size * self.__size
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+        super().__init__(size, size)
