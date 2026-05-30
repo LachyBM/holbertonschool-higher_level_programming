0581add0f59a5be7538c5072f4dda3113cb719be
LachyBM <13072@holbertonstudents.com>
2026-05-30 03:18:45 +0000

    square added

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module for geometry
+of a recet
+"""
+BaseGeometry = __import__("9-rectangle").BaseGeometry
+
+
+class Square(BaseGeometry):
+    """
+    rectangle geo class
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
