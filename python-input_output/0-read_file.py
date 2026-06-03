2d751ea8ebfcd05622810b3452f84caa41a7f3ce
LachyBM <13072@holbertonstudents.com>
2026-06-03 10:51:23 +0000

    basic read

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+module for
+printing whats in
+a file
+"""
+
+
+def read_file(filename=""):
+    """
+    prints file
+    contents
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
