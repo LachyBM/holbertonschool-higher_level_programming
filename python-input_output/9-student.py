91410ddf5e529b47c671e86ad6d1dd9ea924c812
LachyBM <13072@holbertonstudents.com>
2026-06-03 15:22:26 +0000

    added student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module for
+student class
+"""
+
+
+class Student:
+    """
+    class student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
