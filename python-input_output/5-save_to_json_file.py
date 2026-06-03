b1a9dba72547b43cdaa9232bd66b52403d02d78c
LachyBM <13072@holbertonstudents.com>
2026-06-03 15:06:46 +0000

    fixed?

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,5 +10,5 @@ def save_to_json_file(my_obj, filename):
     """
     prints string
     """
-    with open(filename, "a", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
