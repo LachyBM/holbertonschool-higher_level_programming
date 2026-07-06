eec026847fd822778d9df2beef20ccfbdae413c2
LachyBM <13072@holbertonstudents.com>
2026-07-06 04:49:44 +0200

    forgot to check 2nd

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,15 @@
 const argv = process.argv.slice(2);
 const argc = process.argv.length;
 let biggest = 0;
-
+let second = 0;
 if (argc > 3) {
   for (let i = 0; i <= argc; i++) {
     if (Number(argv[i]) >= biggest) {
+      second = biggest;
       biggest = argv[i];
+    } else if (Number(argv[i]) >= second) {
+      second = argv[i];
     }
   }
 }
-console.log(biggest);
+console.log(second);
