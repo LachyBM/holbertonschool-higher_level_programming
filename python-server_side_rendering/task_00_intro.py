3dbeeee4e70cc9ca23860ccc3b8b0cf7a0283772
LachyBM <13072@holbertonstudents.com>
2026-07-17 23:24:28 +1000

    added python ssr

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import os
+
+def generate_invitations(template_content, attendees):
+
+    if type(template_content) is not str:
+        print("Template is not a string")
+        return
+    if template_content == ' ':
+        print( "Template is empty, no output files generated.")
+        return
+    if type(attendees) is not list:
+        print("Attendees needs to be in a list")
+        return
+    if all(isinstance(people, dict) for people in attendees) is False:
+        print("Attendees need to have a dictionary of there data")
+        return
+    if attendees == 0:
+        print("No data provided, no output files generated")
+        return
+    
+    printer = ''
+    count = 0
+    options = ['name', 'event_title', 'event_date','event_location']
+    length = len(options)
+
+    for person in attendees:
+        message = template_content
+        for n in range (0,length):
+            try:
+                x = person[options[n]]
+                if x is None:
+                    raise KeyError     
+            except KeyError:
+                x = options[n]+":N/A"
+            message = message.replace('{'+ options[n] + '}', x)
+
+        printer = message
+
+        printerPath = f"output_{count}.txt"
+        if os.path.exists(printerPath):
+            print("Exists")
+        else:
+            with open(printerPath, "w", encoding="utf-8") as f:
+                f.write(printer)
+
+        count += 1
